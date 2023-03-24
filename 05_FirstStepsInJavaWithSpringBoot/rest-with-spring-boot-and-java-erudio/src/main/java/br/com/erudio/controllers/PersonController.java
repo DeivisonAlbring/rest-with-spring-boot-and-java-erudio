e3135cd6b74a3d9ceac7c1a363212c23c16dbdad
DeivisonAlbring <deivisonalbring@gmail.com>
2023-03-24 14:54:58 -0300

    modulo 13 concluido - content negotiation

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,7 +3,7 @@ package br.com.erudio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import br.com.erudio.util.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +18,7 @@ import br.com.erudio.data.DTO.v1.PersonDTO;
 import br.com.erudio.services.PersonServices;
 
 @RestController
-@RequestMapping("/person/v1")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
@@ -26,27 +26,46 @@ public class PersonController {
 	
 	
 	@GetMapping(value = "/",			
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = {
+					MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public List<PersonDTO> findAll() {		
 		return service.findAll();
 	}
 	
 	
 	@GetMapping(value = "/{id}",			
-			produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = {MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonDTO findById(@PathVariable(value = "id") Long id) throws Exception {		
 		return service.findById(id);
 	}
 	
 	
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(
+			produces = {
+					MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML},
+			consumes = {
+					MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonDTO create(@RequestBody PersonDTO PersonDTO) throws Exception {		
 		return service.create(PersonDTO);
 	}
 		
-	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(
+			produces = {
+					MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML},
+			consumes = {
+					MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonDTO update(@RequestBody PersonDTO PersonDTO) throws Exception {		
 		return service.update(PersonDTO);
 	}
